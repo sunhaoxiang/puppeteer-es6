7d60860cb2763e4d47b251b29e1ecc1f86927a2b
孙昊翔 <673686754@qq.com>
2018-03-22 22:18:39 +0800

    no message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const colors = require('colors');
 puppeteer.launch().then(async browser => {
   let page = await browser.newPage();
   await page.goto('http://es6.ruanyifeng.com/#README', { waitUntil: 'networkidle0' });
+  // 获取所有页面的标题和链接
   let aTags = await page.evaluate(() => {
     let as = [...document.querySelectorAll('#sidebar ol li a')];
     return as.map((a) =>{
@@ -13,10 +14,12 @@ puppeteer.launch().then(async browser => {
       }
     });
   });
+  // 抓取第一页
   await page.pdf({path: `./pdf/1 - ${aTags[0].name}.pdf`});
   console.log(`${aTags[0].name}抓取完成！`);
   await page.close();
 
+  // 从第二页开始循环往后抓取
   for (let i = 1; i < aTags.length; i++) {
     page = await browser.newPage();
     let a = aTags[i];
